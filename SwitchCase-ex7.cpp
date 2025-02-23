b439ab71eb15a5ad831edcfe37a101a5669ba9eb
RoameUMD <136635235+RoameUMD@users.noreply.github.com>
2025-02-23 19:40:46 +0000

    Ex7 Completed

--- SwitchCase-ex7.cpp
+++ SwitchCase-ex7.cpp
@@ -1,1 +1,29 @@
-// add your code here
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    char tmp;
+
+    cout << "Input a number: ";
+    cin >> a;
+
+    if(a < 10 && a % 2 == 0){
+        tmp = 'A';
+    } else if(a % 2 == 1){
+        tmp = 'B';
+    }
+
+    switch (tmp){
+    case 'A':
+        cout << a*a << "\n";
+        break;
+    case 'B':
+        cout << float(a)/2 << "\n";
+        break;
+
+    default:
+        cout << "hahaha\n";
+        break;
+    }
+}
